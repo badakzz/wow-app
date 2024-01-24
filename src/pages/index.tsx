cf2a7381a8e08af931285a47df1bbef4778c28b1
= <lucas.deray@gmail.com>
2024-01-24 19:32:37 +0100

    add bootstrap, fix alignements, create blueprint for the pages to come

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { Container } from 'react-bootstrap'
+import { Container, Image } from 'react-bootstrap'
 
 export default function Home() {
     return (
@@ -12,8 +12,30 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Container>
-                <div>First Commit</div>
+            <Container className="container-top-margin custom-container p-5">
+                <div className="d-flex align-items-center justify-content-center">
+                    <div className="flex-half text-align-start">
+                        <h1 className="mb-5">
+                            WoW Season of Discovery Companion
+                        </h1>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </div>
+                    <div className="flex-half d-flex align-items-center justify-content-center">
+                        <Image
+                            src="/wowsodlogo.png "
+                            alt="wowsodlogo"
+                            width="70%"
+                        ></Image>
+                    </div>
+                </div>
             </Container>
         </>
     )
