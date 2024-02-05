25c5adf5a8856051c26f894fda739c41faf21255
= <lucas.deray@gmail.com>
2024-02-05 10:18:39 +0100

    add python script to scrap items names from wow api, add api endpoints to retrieve item name from id, to retrieve item prices from ah id and item id, to calculate market value diff from id on the two most recent timestamps

---
+++ src/pages/api/v2/[itemId]/index.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+import { sanitizeInput } from '../../../../utils/helpers'
+
+const prisma = new PrismaClient()
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    try {
+        const { itemId } = sanitizeInput(req.query)
+
+        const data = await prisma.item_names.findUnique({
+            where: {
+                itemId: parseInt(itemId),
+            },
+        })
+
+        if (data.length === 0) {
+            res.status(404).json({ error: 'Data not found' })
+        } else {
+            res.status(200).json(data)
+        }
+    } catch (error) {
+        console.error('Request error', error)
+        res.status(500).json({ error: 'Error fetching data' })
+    } finally {
+        await prisma.$disconnect()
+    }
+}
