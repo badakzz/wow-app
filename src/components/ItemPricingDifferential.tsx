4b1747bd7cffd1ed4c12ae7ad238e39d5afefcaa
= <lucas.deray@gmail.com>
2024-02-07 17:01:02 +0100

    create basic component, add helper function to convert prices from tsm api to wow currency

--- src/components/ItemPricingDifferential.tsx
+++ src/components/ItemPricingDifferential.tsx
@@ -1,38 +1,25 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-type ItemPriceDifferentialProps = {
+type ItemPricingDifferentialProps = {
     itemId: number
     auctionHouseId: number
 }
 
-const ItemPriceDifferential: React.FC<ItemPriceDifferentialProps> = ({
+const ItemPricingDifferential: React.FC<ItemPricingDifferentialProps> = ({
     itemId,
     auctionHouseId,
 }) => {
     const [data, setData] = useState<{ difference: number } | null>(null)
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState('')
 
     const fetchPriceDifferential = () => {
-        setIsLoading(true)
         axios
             .get(
                 `/api/v2/${itemId}/${auctionHouseId}/market_value_differential`
             )
             .then((response) => {
                 setData(response.data)
-                setError('')
             })
-            .catch((err) => {
-                setIsLoading(false)
-                if (err.response && err.response.status === 404) {
-                    setError('No data available for this item.')
-                } else {
-                    setError('An error occurred while fetching data.')
-                }
-            })
-            .finally(() => setIsLoading(false))
     }
 
     useEffect(() => {
@@ -43,9 +30,7 @@ const ItemPriceDifferential: React.FC<ItemPriceDifferentialProps> = ({
 
     return (
         <div>
-            {isLoading && <div>Loading...</div>}
-            {!isLoading && error && <div>{error}</div>}
-            {!isLoading && !error && data && (
+            {data && (
                 <div>
                     Market Value :
                     {data.difference >= 0 ? (
@@ -55,9 +40,8 @@ const ItemPriceDifferential: React.FC<ItemPriceDifferentialProps> = ({
                     )}
                 </div>
             )}
-            {!isLoading && !error && !data && <div>No data to display.</div>}
         </div>
     )
 }
 
-export default ItemPriceDifferential
+export default ItemPricingDifferential
