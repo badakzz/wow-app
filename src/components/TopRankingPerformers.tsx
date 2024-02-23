3528c53a388e7b5756c249e631bc7c535738558b
= <lucas.deray@gmail.com>
2024-02-23 16:01:19 +0100

    wip

--- src/components/TopRankingPerformers.tsx
+++ src/components/TopRankingPerformers.tsx
@@ -6,7 +6,7 @@ import { Encounter, Ranking } from '../utils/types'
 import { getRankingClassColor } from '../utils/helpers'
 import { RankingClassPicker } from '.'
 import { RANKING_CLASS } from '@/utils/constants'
-import { Button } from 'react-bootstrap'
+import { Button, Spinner } from 'react-bootstrap'
 
 type TopRankingPerformersTableProps = {
     encounter: Encounter
@@ -19,18 +19,21 @@ const TopRankingPerformersTable: React.FC<TopRankingPerformersTableProps> = ({
     const [hasMorePages, setHasMorePages] = useState<boolean>(false)
     const [pageIndex, setPageIndex] = useState<number>(0)
     const [rankingClass, setRankingClass] = useState<RANKING_CLASS | null>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     const fetchData = async (encounterId: number, page: number) => {
+        setIsLoading(true)
         try {
             const response = await axios.post('/api/v1/warcraftlogs/raids', {
                 operation: 'getEncounterDetails',
                 parameters: { encounterId: encounterId, page: page },
             })
-
             setData(response.data.rankings)
             setHasMorePages(response.data.hasMorePages)
         } catch (error) {
             console.error('Error fetching data:', error)
+        } finally {
+            setIsLoading(false)
         }
     }
 
@@ -50,7 +53,7 @@ const TopRankingPerformersTable: React.FC<TopRankingPerformersTableProps> = ({
                 ),
             },
             {
-                Header: 'Amount',
+                Header: 'DPS',
                 accessor: 'amount' as keyof Ranking,
                 disableFilters: true,
             },
@@ -78,59 +81,77 @@ const TopRankingPerformersTable: React.FC<TopRankingPerformersTableProps> = ({
     }, [encounter, pageIndex])
 
     return (
-        <>
-            <RankingClassPicker
-                rankingClass={rankingClass}
-                setRankingClass={setRankingClass}
-            />
-            <table {...getTableProps()}>
-                <thead>
-                    {headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps()}>
-                                    {column.render('Header')}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map((cell) => {
-                                    return (
-                                        <td {...cell.getCellProps()}>
-                                            {cell.render('Cell')}
-                                        </td>
-                                    )
-                                })}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            <div className="pagination">
-                <Button
-                    className="page-button"
-                    onClick={() => setPageIndex((old) => Math.max(old - 1, 0))}
-                    disabled={pageIndex === 0}
-                >
-                    <span>{'<'}</span>
-                </Button>
-                <Button
-                    className="page-button"
-                    onClick={() =>
-                        setPageIndex((old) => (!hasMorePages ? old : old + 1))
-                    }
-                    disabled={!hasMorePages}
-                >
-                    <span>{'>'}</span>
-                </Button>
+        <div className="d-flex flex-column align-items-center justify-content-center rankings-main-container">
+            <div className="rankings-picker-table-container d-flex flex-column align-items-start">
+                <RankingClassPicker
+                    className="mb-3"
+                    rankingClass={rankingClass}
+                    setRankingClass={setRankingClass}
+                />
+                {isLoading && (
+                    <div className="spinner">
+                        <Spinner animation="border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </Spinner>
+                    </div>
+                )}
             </div>
-        </>
+            {!isLoading && (
+                <div className="d-flex flex-wrap rankings-table-container">
+                    <table {...getTableProps()} className="rankings-table">
+                        <thead>
+                            {headerGroups.map((headerGroup) => (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {headerGroup.headers.map((column) => (
+                                        <th {...column.getHeaderProps()}>
+                                            {column.render('Header')}
+                                        </th>
+                                    ))}
+                                </tr>
+                            ))}
+                        </thead>
+                        <tbody {...getTableBodyProps()}>
+                            {rows.map((row) => {
+                                prepareRow(row)
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {row.cells.map((cell) => {
+                                            return (
+                                                <td {...cell.getCellProps()}>
+                                                    {cell.render('Cell')}
+                                                </td>
+                                            )
+                                        })}
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                    <div className="pagination">
+                        <Button
+                            className="page-button"
+                            onClick={() =>
+                                setPageIndex((old) => Math.max(old - 1, 0))
+                            }
+                            disabled={pageIndex === 0}
+                        >
+                            <span>{'<'}</span>
+                        </Button>
+                        <Button
+                            className="page-button"
+                            onClick={() =>
+                                setPageIndex((old) =>
+                                    !hasMorePages ? old : old + 1
+                                )
+                            }
+                            disabled={!hasMorePages}
+                        >
+                            <span>{'>'}</span>
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
 
