5f73ad47ccdb428db588df4464a8115358a266ce
= <lucas.deray@gmail.com>
2024-02-27 19:43:24 +0100

    add graph component to display quantity/market value/min buyout for an item

--- src/components/ItemLatestPricesGraph.tsx
+++ src/components/ItemLatestPricesGraph.tsx
@@ -1,7 +1,10 @@
+import { formatRawPriceToCopperSilverGold } from '../utils/helpers'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import {
-    LineChart,
+    ResponsiveContainer,
+    ComposedChart,
+    Bar,
     Line,
     XAxis,
     YAxis,
@@ -13,11 +16,18 @@ import {
 type ItemLatestPricesGraphProps = {
     itemId: number
     auctionHouseId: number
+} & any
+
+type CustomTooltipProps = {
+    active?: boolean
+    payload?: any[]
+    label?: string
 }
 
 const ItemLatestPricesGraph: React.FC<ItemLatestPricesGraphProps> = ({
     itemId,
     auctionHouseId,
+    ...restOfProps
 }) => {
     const [data, setData] = useState([])
     console.log(data)
@@ -33,10 +43,46 @@ const ItemLatestPricesGraph: React.FC<ItemLatestPricesGraphProps> = ({
         }
     }
 
-    const formatXAxis = (tickItem) => {
-        // Assuming your snapshotDate is in a format that can be directly passed to Date constructor
+    const CustomTooltip: React.FC<CustomTooltipProps> = ({
+        active,
+        payload,
+        label,
+    }) => {
+        if (active && payload && payload.length) {
+            const marketValue = formatRawPriceToCopperSilverGold(
+                payload[0].value
+            )
+            const marketValueFormatted = marketValue
+                ? `${marketValue.gold}g ${marketValue.silver}s ${marketValue.copper}c`
+                : 'N/A'
+
+            const minBuyout = payload.find((p) => p.dataKey === 'minBuyout')
+            const minBuyoutValue = formatRawPriceToCopperSilverGold(
+                minBuyout?.value
+            )
+            const minBuyoutFormatted = minBuyoutValue
+                ? `${minBuyoutValue.gold}g ${minBuyoutValue.silver}s ${minBuyoutValue.copper}c`
+                : 'N/A'
+
+            const date = new Date(label as string)
+            const time = `${date.getHours()}:${
+                date.getMinutes() < 10 ? '0' : ''
+            }${date.getMinutes()}`
+
+            return (
+                <div className="graph-tooltip">
+                    <div className="label">{`${time}`}</div>
+                    <div className="intro">{`Market Value: ${marketValueFormatted}`}</div>
+                    <div className="intro">{`Min Buyout: ${minBuyoutFormatted}`}</div>
+                </div>
+            )
+        }
+
+        return null
+    }
+
+    const formatXAxis = (tickItem: string) => {
         const date = new Date(tickItem)
-        // Format the date to show hour and minutes. Adjust the format as needed.
         return `${date.getHours()}:${
             date.getMinutes() < 10 ? '0' : ''
         }${date.getMinutes()}`
@@ -51,18 +97,47 @@ const ItemLatestPricesGraph: React.FC<ItemLatestPricesGraphProps> = ({
     return (
         <>
             {data && (
-                <LineChart width={400} height={400} data={data}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="snapshotDate" tickFormatter={formatXAxis} />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line
-                        type="monotone"
-                        dataKey="marketValue"
-                        stroke="#8884d8"
-                    />
-                </LineChart>
+                <ResponsiveContainer width="100%" height={400} {...restOfProps}>
+                    <ComposedChart data={data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis
+                            dataKey="snapshotDate"
+                            tickFormatter={formatXAxis}
+                        />
+                        <YAxis
+                            yAxisId="left"
+                            orientation="left"
+                            stroke="#8884d8"
+                        />
+                        <YAxis
+                            yAxisId="right"
+                            orientation="right"
+                            stroke="#82ca9d"
+                        />
+                        <Tooltip content={<CustomTooltip />} />
+                        <Legend />
+                        <Bar
+                            yAxisId="right"
+                            dataKey="quantity"
+                            fill="rgb(100, 177, 255)"
+                            name="Quantity"
+                        />
+                        <Line
+                            yAxisId="left"
+                            type="monotone"
+                            dataKey="marketValue"
+                            stroke="#a884d8"
+                            name="Market Value"
+                        />
+                        <Line
+                            yAxisId="left"
+                            type="monotone"
+                            dataKey="minBuyout"
+                            stroke="#82ca9d"
+                            name="Minimum Buyout"
+                        />
+                    </ComposedChart>
+                </ResponsiveContainer>
             )}
         </>
     )
