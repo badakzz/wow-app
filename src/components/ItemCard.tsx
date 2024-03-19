5aa6bb1baba060b8915ac40cfe12920c3e15ad2c
= <lucas.deray@gmail.com>
2024-03-19 12:34:37 +0100

    add ellipsis to toggle dropdown for actions on item card

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,27 +1,51 @@
 import { Item } from '../utils/types'
 import { getItemColorByRarity } from '../utils/helpers'
-import { Image } from 'react-bootstrap'
+import { Image, Dropdown } from 'react-bootstrap'
+import { FaEllipsisVertical } from 'react-icons/fa6'
+import { useState } from 'react'
 
 type ItemCardProps = {
     item: Item | null
 }
 
 const ItemCard: React.FC<ItemCardProps> = ({ item }) => {
+    const showDetails = () => console.log('Showing details for', item?.itemName)
+    const deleteItem = () => console.log('Deleting', item?.itemName)
+
     return (
         item && (
-            <div className="d-flex gap-3">
-                <Image
-                    src={item.mediaUrl}
-                    alt="item-icon"
-                    className="item-icon-sm"
-                />
-                <span
-                    style={{
-                        color: getItemColorByRarity(item.itemRarity),
-                    }}
-                >
-                    {item.itemName}
-                </span>
+            <div className="d-flex align-items-center item-card justify-content-between">
+                <div className="d-flex align-items-center gap-3">
+                    <Image
+                        src={item.mediaUrl}
+                        alt="item-icon"
+                        className="item-card-icon"
+                    />
+                    <span
+                        style={{
+                            color: getItemColorByRarity(item.itemRarity),
+                            padding: '0.6rem',
+                        }}
+                    >
+                        {item.itemName}
+                    </span>
+                </div>
+                <Dropdown>
+                    <Dropdown.Toggle
+                        variant="transparent"
+                        className="border-0 bg-transparent p-0"
+                    >
+                        <FaEllipsisVertical />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={showDetails}>
+                            Show details
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={deleteItem}>
+                            Delete
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
         )
     )
