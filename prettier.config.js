6420ea5d990a5ed6d03839ba50724deadb620a49
= <lucas.deray@gmail.com>
2024-04-11 10:46:57 +0200

    fix sensitive data and modify prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,12 +2,18 @@ module.exports = {
     trailingComma: 'es5',
     tabWidth: 4,
     semi: false,
-    singleQuote: true,
+    singleQuote: false,
     overrides: [
         {
             files: '*.scss',
             options: {
                 semi: true,
+            },
+        },
+        {
+            files: ['*.yml', '*.yaml'],
+            options: {
+                tabWidth: 2,
             },
         },
     ],
