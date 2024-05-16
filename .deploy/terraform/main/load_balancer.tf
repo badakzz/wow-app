3372ff5d315e29e598c788dd9aa9d203a74aa606
= <lucas.deray@gmail.com>
2024-05-16 11:55:37 +0200

    enable https on load balancer

--- .deploy/terraform/main/load_balancer.tf
+++ .deploy/terraform/main/load_balancer.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_listener_rule" "wow_app_listener_rule" {
-  listener_arn = data.terraform_remote_state.central.outputs.app_listener_arn
+  listener_arn = data.terraform_remote_state.central.outputs.https_listener_arn
   priority     = 100
 
   action {
